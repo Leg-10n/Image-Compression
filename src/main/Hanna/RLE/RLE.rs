be257a39fb0ed4013948152c7550f5d0354e40a5
81141 <46625054+81141@users.noreply.github.com>
2025-03-21 18:48:59 +0700

    Finished Encoding for RLE0

--- src/main/Hanna/RLE/RLE.rs
+++ src/main/Hanna/RLE/RLE.rs
@@ -5,10 +5,28 @@ fn encode(input: &str) -> String {
     let mut result = String::new();
     let mut chars = input.chars();
     let mut prev = chars.next().unwrap(); // get the first charater 
-    result.push(prev); 
+    let mut counter = 1;
+    for c in chars {
+        if c == prev {
+            counter += 1;
+        } else {
+            result.push(prev);
+            if counter > 1 {
+                result.push_str(&counter.to_string());
+            }
+            prev = c;
+            counter = 1;
+        }
+    }
+    result.push(prev);
+    if count > 1 {
+        result.push_str(&count.to_string());
+    }
     result
 }
-
+fn decode(input: &str) ->String {
+    ???
+}
 fn main() {
     println!("Test TEST");
 }
